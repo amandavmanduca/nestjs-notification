a43a86f81e122b26b57202cef41cc4d6015ebc40
Amanda Manduca <amandavmanduca@hotmail.com>
2022-12-18 19:29:51 -0300

    :art: Improve in memory notifications repository

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,3 +1,4 @@
+import { NotificationNotFound } from '@app/use-cases/errors/notification-not-found';
 import { Notification } from '../../src/app/entities/notification';
 import { NotificationsRepository } from '../../src/app/repositories/notifications-repository';
 
@@ -5,6 +6,24 @@ export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification> {
+    const foundNotification = await this.notifications.find(
+      (n) => n.id === notificationId
+    );
+    if (!foundNotification) {
+      throw new NotificationNotFound();
+    }
+    return foundNotification;
+  }
+  async save(notification: Notification): Promise<void> {
+    const foundNotification = await this.findById(notification.id);
+    const notifications = [
+      ...this.notifications.filter((n) => n.id !== foundNotification.id),
+      notification
+    ];
+    this.notifications = notifications;
+  }
 
   async create(notification: Notification) {
     this.notifications.push(notification);
